8bffa0c80033ce53d0b01f2fda1deeb349e22c78
Akshit Gupta <akshit.gupta@snapdeal.com>
2016-03-10 17:12:53 +0530

    first commit

---
+++ app/src/main/java/project0/udacity/akshit/project0/MainActivityFragment.java
@@ -0,0 +1,31 @@
+package project0.udacity.akshit.project0;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    ArrayAdapter<String> weatherFroecastAdapter;
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
